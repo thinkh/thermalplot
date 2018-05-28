7a396d9d1fa114140255130082d1d8943e98286f
Holger Stitz <holger.stitz@jku.at>
2018-05-29 00:51:33 +0200

    Convert OHLCV into USD

--- client/data/thermal_crypto/server/index.js
+++ client/data/thermal_crypto/server/index.js
@@ -10,7 +10,7 @@ const TABLE_CRYPTO_PRICES = 'crypto_prices';
 const TABLE_CRYPTO_PRICES_FIELDS = ['opening_price', 'highest_price', 'lowest_price', 'closing_price', 'volume_crypto', 'volume_btc'];
 
 const TABLE_BTC_PRICES = 'btc_prices';
-const TABLE_BTC_PRICES_FIELDS = ['ts', 'currency_code', 'opening_price', 'highest_price', 'lowest_price', 'closing_price', 'volume_currency', 'volume_btc'];
+const TABLE_BTC_PRICES_FIELDS = ['currency_code', 'opening_price', 'highest_price', 'lowest_price', 'closing_price', 'volume_currency', 'volume_btc'];
 
 
 function open_db() {
@@ -32,7 +32,7 @@ class SocketHandler extends UseCaseDBSocketHandler {
         super(socket, 24 * 60 * 60);  // TIME FACTOR in sec = 1 day
         this.dateformat = 'Y-m-d';
 
-        this.filter_in = ['ETH', 'LTC', 'IOT', 'XLM'];
+        //this.filter_in = ['ETH', 'LTC', 'IOT', 'XLM'];
     }
 
     open_db() {
@@ -52,11 +52,25 @@ class SocketHandler extends UseCaseDBSocketHandler {
     read_data(start, end, time_factor) {
         start *= time_factor;
         end *= time_factor;
+        const currency = 'USD'; // fiat currency from TABLE_BTC_PRICES
 
-        logger.info('read data between [%s (%d s)] and [%s (%d s)] with timeFactor: %s', to_time(start), start, to_time(end), end, time_factor);
-        const sql = (`select currency_code, ts, ` + TABLE_CRYPTO_PRICES_FIELDS.join(',') +
-            ' from ' + TABLE_CRYPTO_PRICES + ' s where s.ts >= ? and s.ts < ?  ' + this.filter('currency_code') +
-            ' order by s.ts asc')
+        logger.info('read data between [%s (%d s)] and [%s (%d s)] with timeFactor: %s', new Date(start * 1000).toUTCString(), start, new Date(end * 1000).toUTCString(), end, time_factor);
+        const sql = `
+            SELECT
+                cp.currency_code,
+                cp.ts,
+                ${TABLE_CRYPTO_PRICES_FIELDS.slice(0, -2).map((field) => `cp.${field}*bp.${field} as ${field}`).join(',')},
+                cp.volume_crypto,
+                cp.volume_btc,
+                cp.volume_btc*bp.closing_price as volume_currency
+            FROM ${TABLE_CRYPTO_PRICES} cp
+            LEFT JOIN ${TABLE_BTC_PRICES} bp
+            ON cp.ts = bp.ts
+            WHERE
+                bp.currency_code = '${currency}'
+                AND cp.ts >= ? and cp.ts < ?
+                ${this.filter('currency_code')}
+            ORDER BY cp.ts ASC`;
 
         const rows = this.db.prepare(sql).all(start, end);
 
@@ -105,7 +119,7 @@ class CSVHandler {
         close_db(db);
 
         // write csv header
-        this.res.write(TABLE_BTC_PRICES_FIELDS.join(',') + '\n');
+        this.res.write('date,' + TABLE_BTC_PRICES_FIELDS.join(',') + '\n');
 
         // write csv data
         rows.map((row) => Object.values(row))

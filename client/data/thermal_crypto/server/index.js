444c460cc7a13e327b906f568041c416ab23eb2d
Holger Stitz <holger.stitz@jku.at>
2018-05-28 20:02:25 +0200

    Change SQLite column name from date to ts and use `date(ts,'unixepoch')` if necessary

--- client/data/thermal_crypto/server/index.js
+++ client/data/thermal_crypto/server/index.js
@@ -10,7 +10,7 @@ const TABLE_CRYPTO_PRICES = 'crypto_prices';
 const TABLE_CRYPTO_PRICES_FIELDS = ['opening_price', 'highest_price', 'lowest_price', 'closing_price', 'volume_crypto', 'volume_btc'];
 
 const TABLE_BTC_PRICES = 'btc_prices';
-const TABLE_BTC_PRICES_FIELDS = ['date', 'currency_code', 'opening_price', 'highest_price', 'lowest_price', 'closing_price', 'volume_currency', 'volume_btc'];
+const TABLE_BTC_PRICES_FIELDS = ['ts', 'currency_code', 'opening_price', 'highest_price', 'lowest_price', 'closing_price', 'volume_currency', 'volume_btc'];
 
 
 function open_db() {
@@ -40,44 +40,30 @@ class SocketHandler extends UseCaseDBSocketHandler {
     }
 
     find_first_ts() {
-        const row = this.db.prepare('select min(s.date) as date from ' + TABLE_CRYPTO_PRICES + ' s where 1=1 ' + this.filter('currency_code')).get();
-        return Date.parse(row.date) / 1000; // [ms -> sec]
+        const row = this.db.prepare('select min(s.ts) as ts from ' + TABLE_CRYPTO_PRICES + ' s where 1=1 ' + this.filter('currency_code')).get();
+        return row.ts; // [sec]
     }
 
     find_last_ts() {
-        const row = this.db.prepare('select max(s.date) from ' + TABLE_CRYPTO_PRICES + ' s where 1=1 ' + this.filter('currency_code')).get();
-        return Date.parse(row.date) / 1000; // [ms -> sec]
+        const row = this.db.prepare('select max(s.ts) as ts from ' + TABLE_CRYPTO_PRICES + ' s where 1=1 ' + this.filter('currency_code')).get();
+        return row.ts; // [sec]
     }
 
     read_data(start, end, time_factor) {
         start *= time_factor;
         end *= time_factor;
 
-        function to_time(t) {
-            const date = new Date();
-            date.setTime(t * 1000); // [sec -> ms]
-            let r = date.getFullYear() + '-'
-            if (date.getMonth() < 10) {
-                r += '0'
-            }
-            r += date.getMonth() + 1; // +1 because JS counts month form 0
-            r += '-';
-            const day = date.getDate();
-            r += (day < 10) ? '0' + day : day;
-            return r;
-        }
-
         logger.info('read data between [%s (%d s)] and [%s (%d s)] with timeFactor: %s', to_time(start), start, to_time(end), end, time_factor);
-        const sql = ('select currency_code, date, ' + TABLE_CRYPTO_PRICES_FIELDS.join(',') +
-            ' from ' + TABLE_CRYPTO_PRICES + ' s where s.date >= ? and s.date < ?  ' + this.filter('currency_code') +
-            ' order by s.date asc')
+        const sql = (`select currency_code, ts, ` + TABLE_CRYPTO_PRICES_FIELDS.join(',') +
+            ' from ' + TABLE_CRYPTO_PRICES + ' s where s.ts >= ? and s.ts < ?  ' + this.filter('currency_code') +
+            ' order by s.ts asc')
 
-        const rows = this.db.prepare(sql).all(to_time(start), to_time(end));
+        const rows = this.db.prepare(sql).all(start, end);
 
         return rows.map((row) => {
             const r = {
                 nip: row.currency_code,
-                ts: Date.parse(row.date) / 1000, // [ms -> sec] row.ts*time_factor
+                ts: row.ts, // [sec] // row.ts*time_factor
                 attrs: {}
             };
 
@@ -104,11 +90,14 @@ class CSVHandler {
             order_by = 'asc';
         }
 
+        start = Date.parse(start) / 1000; // [ms -> sec]
+        end = Date.parse(end) / 1000; // [ms -> sec]
+
         const db = open_db();
 
-        const sql = ('select ' + TABLE_BTC_PRICES_FIELDS.join(', ') +
-            ' from ' + TABLE_BTC_PRICES + ' where date >= ? and date < ? and currency_code = ?' +
-            ' order by date ' + order_by);
+        const sql = (`select date(ts,'unixepoch')` + TABLE_BTC_PRICES_FIELDS.join(', ') +
+            ' from ' + TABLE_BTC_PRICES + ' where ts >= ? and ts < ? and currency_code = ?' +
+            ' order by ts ' + order_by);
 
         logger.info('execute query with %s %s %s', field, start, end);
         const rows = db.prepare(sql).all(start, end, field);
